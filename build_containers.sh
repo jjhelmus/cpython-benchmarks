d64d6b5ce2ff99002d360239bbd7d3ffe4a2ea54
Jonathan Helmus <jjhelmus@gmail.com>
2026-01-19 11:32:21 -0600

    add Debian and Ubuntu containers

--- build_containers.sh
+++ build_containers.sh
@@ -5,3 +5,7 @@ docker build containers/pbs-313 --tag 'local-benchmarks-pbs-313'
 docker build containers/pbs-314 --tag 'local-benchmarks-pbs-314'
 
 docker build containers/fedora --tag 'local-benchmarks-fedora'
+
+docker build containers/debian --tag 'local-benchmarks-debian'
+docker build containers/ubuntu --tag 'local-benchmarks-ubuntu'
+docker build containers/ubuntu --tag 'local-benchmarks-ubuntu-lts'
--- run_linux_benchmarks.sh
+++ run_linux_benchmarks.sh
@@ -5,3 +5,7 @@ docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_singl
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_single.sh pbs-314
 
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora
+
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian ./run_single.sh debian
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu ./run_single.sh ubuntu
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu-lts ./run_single.sh ubuntu-lts
