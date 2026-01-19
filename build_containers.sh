80a66859d48b84f3be5b951a3a8991e93fe5f42d
Jonathan Helmus <jjhelmus@gmail.com>
2026-01-19 11:20:15 -0600

    Add python-build-standalone linux containers

--- build_containers.sh
+++ build_containers.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+docker build containers/pbs-312 --tag 'local-benchmarks-pbs-312'
+docker build containers/pbs-313 --tag 'local-benchmarks-pbs-313'
+docker build containers/pbs-314 --tag 'local-benchmarks-pbs-314'
+
 docker build containers/fedora --tag 'local-benchmarks-fedora'
--- run_linux_benchmarks.sh
+++ run_linux_benchmarks.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-312 ./run_single.sh pbs-312
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_single.sh pbs-313
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_single.sh pbs-314
+
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora
