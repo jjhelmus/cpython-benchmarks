5da1a082e651fc9b145ee61dcf942e191d1dcc66
Jonathan Helmus <jjhelmus@gmail.com>
2026-01-19 11:54:34 -0600

    add conda-forge container

--- build_containers.sh
+++ build_containers.sh
@@ -4,6 +4,8 @@ docker build containers/pbs-312 --tag 'local-benchmarks-pbs-312'
 docker build containers/pbs-313 --tag 'local-benchmarks-pbs-313'
 docker build containers/pbs-314 --tag 'local-benchmarks-pbs-314'
 
+docker build containers/conda-forge --tag 'local-benchmarks-conda-forge'
+
 docker build containers/fedora --tag 'local-benchmarks-fedora'
 docker build containers/rocky --tag 'local-benchmarks-rocky'
 
--- run_linux_benchmarks.sh
+++ run_linux_benchmarks.sh
@@ -4,6 +4,8 @@ docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-312 ./run_singl
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_single.sh pbs-313
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_single.sh pbs-314
 
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-conda-forge conda run --prefix /bench_env --live-stream ./run_single.sh conda-forge
+
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-rocky ./run_single.sh rocky
 
