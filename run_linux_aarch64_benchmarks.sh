01bcd4663779305ea45b3499929d17e1df66dccc
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-02-05 13:28:30 +0000

    add linux aarch64 script

---
+++ run_linux_aarch64_benchmarks.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-312 ./run_single.sh pbs-312
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_single.sh pbs-313-aarch64
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_single.sh pbs-314-aarch64
+
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-conda-forge conda run --prefix /bench_env --live-stream ./run_single.sh conda-forge-aarch64
+
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora-aarch64
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-rocky ./run_single.sh rocky
+
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian ./run_single.sh debian-aarch64
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu ./run_single.sh ubuntu
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu-lts ./run_single.sh ubuntu-lts
