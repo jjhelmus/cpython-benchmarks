8cd4e86b17255ff8d7aa5a6beee2b299652f5e27
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-07-10 12:20:12 -0500

    only build+run 3.14 benchmarks

--- build_containers.sh
+++ build_containers.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker build containers/pbs-312 --tag 'local-benchmarks-pbs-312'
-docker build containers/pbs-313 --tag 'local-benchmarks-pbs-313'
+# Only build 3.14 containers
+
+#docker build containers/pbs-312 --tag 'local-benchmarks-pbs-312'
+#docker build containers/pbs-313 --tag 'local-benchmarks-pbs-313'
 docker build containers/pbs-314 --tag 'local-benchmarks-pbs-314'
 
 docker build containers/conda-forge --tag 'local-benchmarks-conda-forge'
@@ -9,9 +11,9 @@ docker build containers/conda-forge --tag 'local-benchmarks-conda-forge'
 docker build containers/python --tag 'local-benchmarks-python'
 
 docker build containers/fedora --tag 'local-benchmarks-fedora'
-docker build containers/rocky --tag 'local-benchmarks-rocky'
+#docker build containers/rocky --tag 'local-benchmarks-rocky'
 
-docker build containers/debian --tag 'local-benchmarks-debian'
+#docker build containers/debian --tag 'local-benchmarks-debian'
 docker build containers/debian-forky --tag 'local-benchmarks-debian-forky'
-docker build containers/ubuntu --tag 'local-benchmarks-ubuntu'
+#docker build containers/ubuntu --tag 'local-benchmarks-ubuntu'
 docker build containers/ubuntu-lts --tag 'local-benchmarks-ubuntu-lts'
--- run_linux_benchmarks.sh
+++ run_linux_benchmarks.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-312 ./run_single.sh pbs-312
-docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_single.sh pbs-313
+# Only run 3.14 benchmarks
+
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-312 ./run_single.sh pbs-312
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-313 ./run_single.sh pbs-313
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_single.sh pbs-314
 
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-conda-forge conda run --prefix /bench_env --live-stream ./run_single.sh conda-forge
@@ -9,9 +11,9 @@ docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-conda-forge conda r
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-python ./run_single.sh python-docker
 
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora
-docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-rocky ./run_single.sh rocky
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-rocky ./run_single.sh rocky
 
-docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian ./run_single.sh debian
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian ./run_single.sh debian
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian-forky ./run_single.sh debian-forky
-docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu ./run_single.sh ubuntu
+#docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu ./run_single.sh ubuntu
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu-lts ./run_single.sh ubuntu-lts
