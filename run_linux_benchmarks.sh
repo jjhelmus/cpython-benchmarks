a49958a8b7605060c5d30bcc0f7a9ef797bf2f54
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-07-10 12:19:17 -0500

    add new container to linux runner

--- run_linux_benchmarks.sh
+++ run_linux_benchmarks.sh
@@ -6,9 +6,12 @@ docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-pbs-314 ./run_singl
 
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-conda-forge conda run --prefix /bench_env --live-stream ./run_single.sh conda-forge
 
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-python ./run_single.sh python-docker
+
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-fedora ./run_single.sh fedora
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-rocky ./run_single.sh rocky
 
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian ./run_single.sh debian
+docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-debian-forky ./run_single.sh debian-forky
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu ./run_single.sh ubuntu
 docker run --rm -v ${PWD}:/io --workdir /io local-benchmarks-ubuntu-lts ./run_single.sh ubuntu-lts
