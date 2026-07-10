312c11b57ad4b72bfd1794bd1b62706759de5fe5
Jonathan J. Helmus <jjhelmus@gmail.com>
2026-07-10 12:11:57 -0500

    update docker containers
    fedora 43 -> 44
    ubuntu-lts 24.04 -> 26.04
    add debian-forky
    add python (python:3.14)

--- build_containers.sh
+++ build_containers.sh
@@ -6,9 +6,12 @@ docker build containers/pbs-314 --tag 'local-benchmarks-pbs-314'
 
 docker build containers/conda-forge --tag 'local-benchmarks-conda-forge'
 
+docker build containers/python --tag 'local-benchmarks-python'
+
 docker build containers/fedora --tag 'local-benchmarks-fedora'
 docker build containers/rocky --tag 'local-benchmarks-rocky'
 
 docker build containers/debian --tag 'local-benchmarks-debian'
+docker build containers/debian-forky --tag 'local-benchmarks-debian-forky'
 docker build containers/ubuntu --tag 'local-benchmarks-ubuntu'
 docker build containers/ubuntu-lts --tag 'local-benchmarks-ubuntu-lts'
